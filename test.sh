907074b67af132d80fdef4dd4b1398017b290b5b
Noah Watkins <noahwatkins@gmail.com>
2016-04-04 13:23:26 -0700

    test: add test params
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- test.sh
+++ test.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 
-(docker run --rm --cidfile=/tmp/cid -e NVERTS=1000000 -e INSTANCES=3 -e MEMGB=10 -e CORES=1 docker-test &)
+docker=$1
+shift
+inst=$1
+shift
+cores=$1
+shift
+memgb=$1
+shift
+nverts=$*
 
-sleep 10
-cid=`cat /tmp/cid`
-pid=`docker inspect --format '{{ .State.Pid }}' $cid`
-
-if [ $? -ne 0 ]; then
-  echo "ERROR: unable to get container's PID. Does it run OK?"
+if [ -z "$nverts" ]; then
+  echo "USAGE: ./test.sh <docker img> <instances> <cores> <mem>g nvert[ nvert[ ...]]"
   exit 1
 fi
 
-while ps -p $pid > /dev/null ; do
-  sleep 10
-  if [ -f /cgroup/memory/docker/${cid}/memory.max_usage_in_bytes ] ; then
-    max_mem=`cat /cgroup/memory/docker/${cid}/memory.max_usage_in_bytes`
-    echo "max mem update: $max_mem"
-  fi
-done
+echo "DOCKER IMG: $docker"
+echo "INSTANCES: $inst"
+echo "CORES/INST: $cores"
+echo "GB/INST: ${memgb}g"
+echo "NVERTS: $nverts"
 
-echo "FINAL maxmem=$max_mem bytes / $(($max_mem / 1073741824)) gb" 
-rm /tmp/cid
+for nvert in $nverts; do
+
+  (docker run --rm --cidfile=/tmp/cid -e NVERTS=$nvert -e INSTANCES=$inst -e MEMGB=$memgb -e CORES=$cores $docker &)
+
+  sleep 10
+  cid=`cat /tmp/cid`
+  pid=`docker inspect --format '{{ .State.Pid }}' $cid`
+
+  if [ $? -ne 0 ]; then
+    echo "ERROR: unable to get container's PID. Does it run OK?"
+    exit 1
+  fi
+
+  while ps -p $pid > /dev/null ; do
+    sleep 10
+    if [ -f /cgroup/memory/docker/${cid}/memory.max_usage_in_bytes ] ; then
+      max_mem=`cat /cgroup/memory/docker/${cid}/memory.max_usage_in_bytes`
+      echo "max mem update: $max_mem"
+    fi
+  done
+
+  echo "FINAL maxmem=$max_mem bytes / $(($max_mem / 1073741824)) gb" 
+  rm /tmp/cid
+
+done
